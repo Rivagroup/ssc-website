9121ba7ddf946edce5405e9ad573a1fb4d803d9d
Oliver Oehme <oliver.oehme@gmail.com>
2020-11-16 12:33:40 +0100

    SEO start page

--- application/views/start/start_content.php
+++ application/views/start/start_content.php
@@ -20,95 +20,95 @@
         <div class="carousel-item active">
             <img class="d-block w-100" src="/assets/images/slider_silvester2020.png" alt="Silvester 2020">
             <div class="carousel-caption">
-                <a href="<?= base_url('/silvester'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/silvester'); ?>" class="stretched-link"><span class="d-none">Silvester 2020/21 im Spielcenter Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_bowling.jpg" alt="Bowling Chemnitz">
             <div class="carousel-caption">
                 <h2 class="carousel-caption-title">BOWLING<br>CHEMNITZ</h2>
-                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"><span class="d-none">Bowling in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_bowling.png" alt="Bowling Chemnitz">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"><span class="d-none">Bowling in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_bowling2.png" alt="Bowling Chemnitz">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"><span class="d-none">Bowling in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_bowling3 kamin_.png" alt="Bowling Chemnitz">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"><span class="d-none">Bowling in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_bowling4.png" alt="Bowling Chemnitz">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/bowling'); ?>" class="stretched-link"><span class="d-none">Bowling in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_escape.jpg" alt="Escaperoom Chemnitz">
             <div class="carousel-caption">
                 <h2 class="carousel-caption-title">ESCAPEROOM<br>CHEMNITZ</h2>
-                <a href="<?= base_url('/attraktion/escaperoom'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/escaperoom'); ?>" class="stretched-link"><span class="d-none">Exitgame Escaperoom in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <a href="/silvester" target="_blank"><img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_esc_knast.png" alt="Escaperoom Chemnitz"></a>
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/escaperoom'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/escaperoom'); ?>" class="stretched-link"><span class="d-none">Exitgame Escaperoom in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_esc_psych.png" alt="Escaperoom Chemnitz">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/escaperoom'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/escaperoom'); ?>" class="stretched-link"><span class="d-none">Exitgame Escaperoom in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_silvester2020_2.png" alt="Silvester 2020">
             <div class="carousel-caption">
-                <a href="<?= base_url('/silvester'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/silvester'); ?>" class="stretched-link"><span class="d-none">Silvester 2020/21 im Spielcenter Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_golf.jpg" alt="Minigolf Chemnitz">
             <div class="carousel-caption">
                 <h2 class="carousel-caption-title">ADVENTURE<br>MINIGOLF<br>CHEMNITZ</h2>
-                <a href="<?= base_url('/attraktion/minigolf'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/minigolf'); ?>" class="stretched-link"><span class="d-none">Minigolf in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_minigolf.png" alt="Minigolf Chemnitz">
+            <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_minigolf.png" alt="Minigolf Chemnitz Spielfeld 1">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/minigolf'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/minigolf'); ?>" class="stretched-link"><span class="d-none">Minigolf in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_minigolf2.png" alt="Minigolf Chemnitz">
+            <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_minigolf2.png" alt="Minigolf Chemnitz Spielfeld 2">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/minigolf'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/minigolf'); ?>" class="stretched-link"><span class="d-none">Minigolf in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
             <img class="d-block w-100" src="/assets/images/slider_laser.jpg" alt="Lasergame Chemnitz">
             <div class="carousel-caption">
                 <h2 class="carousel-caption-title">LASERGAME<br>CHEMNITZ</h2>
-                <a href="<?= base_url('/attraktion/lasertag'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/lasertag'); ?>" class="stretched-link"><span class="d-none">Lasergame Lasertag in Chemnitz</span></a>
             </div>
         </div>
         <div class="carousel-item">
-            <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_lasertag.png" alt="Lasergame Chemnitz">
+            <img class="d-block w-100" src="/assets/images/slider_fotos spielcenter_lasertag.png" alt="Lasergame Chemnitz Spielfeld">
             <div class="carousel-caption">
-                <a href="<?= base_url('/attraktion/lasertag'); ?>" class="stretched-link"></a>
+                <a href="<?= base_url('/attraktion/lasertag'); ?>" class="stretched-link"><span class="d-none">Lasergame Lasertag in Chemnitz</span></a>
             </div>
         </div>
     </div>
@@ -121,19 +121,20 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
-<h1 class="mt-3">Spielcenter Chemnitz</h1>
-<p>In unserem Haus bieten wir zahlreiche Möglichkeiten, Ihre
-Veranstaltung zu Ihrer vollsten Zufriedenheit umzusetzen.
-Jedes Event ist einzigartig und bedarf einer individuellen Planung.
+<h1 class="mt-3">Freizeit erleben im Spielcenter Chemnitz</h1>
+<p>In unserem Freizeitzentrum bieten wir zahlreiche Möglichkeiten, Ihre
+Veranstaltung zu Ihrer vollsten Zufriedenheit umzusetzen. Ob Firmenfeier,
+Teambuilding, Kindergeburtstag, Jungesellenabschied oder Familienfeier - 
+jedes Event ist einzigartig und bedarf einer individuellen Planung.
 Damit das möglich ist, haben wir ein modulares System entwickelt,
 welches es uns ermöglicht, noch flexibler auf Ihre speziellen
 Bedürfnisse einzugehen. Hinterlassen Sie bei Ihren Freunden, Geschäftspartnern oder der
 Familie einen bleibenden Eindruck. Wir helfen Ihnen dabei!</p>
-
 <div class="card-deck mt-3">
     <div class="card">
         <div class="bg-white filtereffect h-100 d-flex flex-column justify-content-between">
             <img class="card-img-top" src="/assets/images/startseite_bowling.png" alt="teaser bowling" />
+            <h2 class="card-header h5">Bowling Chemnitz</h2>
             <div class="card-body small">
             Das Bowling Center für Chemnitz/Sachsen ✓ Günstig bowlen auf der größten Bowlingbahn in Chemnitz ☆, Bereits über 20000 zufriedene Bowling - Gäste!
             </div>
@@ -146,8 +147,9 @@ Familie einen bleibenden Eindruck. Wir helfen Ihnen dabei!</p>
     <div class="card">
         <div class="bg-white filtereffect h-100 d-flex flex-column justify-content-between">
             <img class="card-img-top" src="/assets/images/startseite_escape.png" alt="teaser escape">
+            <h2 class="card-header h5">Escaperoom Chemnitz</h2>
             <div class="card-body small">
-            Escaperoom Chemnitz Lasst im Escaperoom Chemnitz den grauen Alltag hinter Euch. Taucht ein in unsere geheimnisvollen Spiele und werdet Teil eines unvergesslichen Erlebnisses!
+            Lasst im Escaperoom Chemnitz den grauen Alltag hinter Euch. Taucht ein in unsere geheimnisvollen Spiele und werdet Teil eines unvergesslichen Erlebnisses!
             </div>
             <div class="card-footer">
                 <a href="https://escaperoom-chemnitz.de" class="btn d-block btn-primary" target="_blank">Escaperoom Chemnitz <i class="small fas fa-external-link-alt"></i></a>
@@ -158,6 +160,7 @@ Familie einen bleibenden Eindruck. Wir helfen Ihnen dabei!</p>
     <div class="card">
         <div class="bg-white filtereffect h-100 d-flex flex-column justify-content-between">
             <img class="card-img-top" src="/assets/images/startseite_lasertag.png" alt="teaser laser">
+            <h2 class="card-header h5">Lasergame Chemnitz</h2>
             <div class="card-body small">
             Höchster Spaßfaktor und absolutes Highlight auf 900m² Spielfläche. Das Beste was Chemnitz zu bieten hat. Sitzt du noch, oder spielst du schon? Nichts für Couch-Potatos!
             </div>
@@ -170,6 +173,7 @@ Familie einen bleibenden Eindruck. Wir helfen Ihnen dabei!</p>
     <div class="card">
         <div class="bg-white filtereffect h-100 d-flex flex-column justify-content-between">
             <img class="card-img-top" src="/assets/images/startseite_minigolf.png" alt="teaser minigolf">
+            <h2 class="card-header h5">Minigolf Chemnitz</h2>
             <div class="card-body small">
             Ihr bewegt euch spielerisch auf einem 16-Loch-Indoor-Minigolf-Parcours durch 4 unterschiedliche Fantasiewelten. Die gesamte Spielfläche wurde mit Wandmotiven in fluoreszierenden Farben gestaltet, die im Schwarzlicht gut zur Geltung kommen.
             </div>
