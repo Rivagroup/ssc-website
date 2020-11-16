debf1e6aec7ca0b7832ffe72ebc6c41ce9c39dc3
Oliver Oehme <oliver.oehme@gmail.com>
2020-11-16 12:57:33 +0100

    seo improvements

--- application/controllers/Start.php
+++ application/controllers/Start.php
@@ -19,7 +19,7 @@ class Start extends MY_ContentController
         foreach ($this->meta as $meta => $daten) {
             $data[$meta] = $daten;
         }
-        $data['title'] .= '- Das Freizeitzentrum in Chemnitz';
+        $data['title'] .= '- Das Spiel- und Freizeitzentrum in Chemnitz.';
         $data['main_content'] = 'start/start_content';
         $this->load->view('template', $data);
     }
